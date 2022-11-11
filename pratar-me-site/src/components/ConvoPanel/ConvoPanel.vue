6ef9805a26acbddbede439967046f671f5ad1694
Melidee <w.ardecan@gmail.com>
2022-11-11 18:24:45 +0000

    deleted default Vue Code

---
+++ pratar-me-site/src/components/ConvoPanel/ConvoPanel.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template class="template">
+  <main>
+    <p>this is a convo panel</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aqua;
+}
+</style>
