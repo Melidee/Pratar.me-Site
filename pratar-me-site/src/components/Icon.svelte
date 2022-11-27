887e4685c1e6b829a7a291f280b014d29edb6714
Melidee <w.ardecan@gmail.com>
2022-11-27 20:53:19 +0000

    generic icon

---
+++ pratar-me-site/src/components/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let src: string;
+  export let diameter: string;
+  const defaultImage =
+    "https://us.123rf.com/450wm/gudinny/gudinny1911/gudinny191100227/gudinny191100227.jpg?ver=6";
+  if (src == undefined) {
+    src = defaultImage;
+  }
+</script>
+
+<main>
+  <img {src} alt="icon" style="height:{diameter};width:{diameter}" />
+</main>
+
+<style>
+  img {
+    padding: 1px;
+    border-radius: 100vw;
+    border: 0.2vh solid #ffffff;
+  }
+</style>
