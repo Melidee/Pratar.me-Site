dc2d8091a1d0b5abf2673f10692d9b93f650e229
Melidee <w.ardecan@gmail.com>
2022-11-27 20:53:45 +0000

    convopanel components

---
+++ pratar-me-site/src/components/ConvoPanel/ConvoButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import type { Conversation } from "../../types";
+  import Icon from "../Icon.svelte";
+  export let convo: Conversation;
+</script>
+
+<main>
+  <div class="icon">
+    <Icon src={convo.icon} diameter="50px"/>
+  </div>
+  <h4>{convo.title}</h4>
+</main>
+
+<style>
+  main {
+    border-radius: 12px;
+    width: 100%;
+    background-color: #272727;
+    margin-top: 0px;
+    height: 75px;
+  }
+
+  .icon {
+    margin-top: 15px;
+  }
+
+  h4 {
+  }
+</style>
---
+++ pratar-me-site/src/components/ConvoPanel/ConvoPanel.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Conversation } from "../../types";
+  import ConvoButton from "./ConvoButton.svelte";
+  export let convos: Array<Conversation> = [];
+</script>
+
+<main>
+  {#each convos as convo}
+    <ConvoButton {convo} />
+  {/each}
+</main>
+
+<style>
+  main {
+    margin-top: 0px;
+    background: rgb(102, 0, 255);
+    background: linear-gradient(
+      180deg,
+      rgba(102, 0, 255, 1) 0%,
+      rgba(102, 0, 255, 1) 50%,
+      rgba(255, 255, 255, 1) 100%
+    );
+    height: inherit;
+    border-radius: 12px;
+    padding: 12px;
+  }
+</style>
